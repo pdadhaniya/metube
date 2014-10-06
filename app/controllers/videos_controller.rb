bed9b94d92c4c9f535c5fcf9499fb4654dea029b
Parag Dadhaniya <paragdadhaniya@gmail.com>
2014-10-06 16:27:52 -0700

    set up first controller for videos

---
+++ app/controllers/videos_controller.rb
@@ -0,0 +1,33 @@
+class VideosController < ApplicationController
+
+  #list all videos - GET /videos
+  def index
+    @videos = Video.all
+  end
+
+  #show a single video - GET /videos/:id
+  def show
+  end
+
+  #new video creation form - Get /videos/new
+  def new
+  end
+
+  #create a new video - POST /videos
+  def create
+  end
+
+  #edit video form - GET /videos/:id/edit
+  def edit
+  end
+
+  #update an existing video - PUT /videos/:id
+  def update
+  end
+
+  #destroy an existing video - DELETE /videos/:id
+  def destroy
+  end
+
+
+end
