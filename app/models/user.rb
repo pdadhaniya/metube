6f509fa6bc94e046c32044e4e3d17df03b103865
Parag Dadhaniya <paragdadhaniya@gmail.com>
2014-10-06 15:37:46 -0700

    add validations to user and video models and has_many and belongs_to relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ActiveRecord::Base
+  has_many :videos, foreign_key; "author_id"
+  validates :email, uniqueness: true
 end
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,3 +1,4 @@
 class Video < ActiveRecord::Base
   belongs_to :author, class_name: "User"
+  validates :author_id, presence: true
 end
