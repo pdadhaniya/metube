61834df8f469356dda17bc90406a4bf789a398cc
Parag Dadhaniya <paragdadhaniya@gmail.com>
2014-10-08 14:07:48 -0700

    add gem devise

--- Gemfile
+++ Gemfile
@@ -26,7 +26,7 @@ gem 'sdoc', '~> 0.4.0',          group: :doc
 gem 'spring',        group: :development
 
 # Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt', '~> 3.1.7'
 
 # Use unicorn as the app server
 # gem 'unicorn'
@@ -41,3 +41,5 @@ gem 'rspec-rails'
 gem 'pry-byebug'
 
 gem 'foundation-rails'
+
+gem 'devise'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  validates :name, presence: true
+  validates :email, uniqueness: true
   has_many :videos, foreign_key: "author_id"
   has_many :playlists, foreign_key: "author_id"
-  validates :email, uniqueness: true
 end
